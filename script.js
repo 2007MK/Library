1eaeee1a168ed5d71e97ff189cdc0406862693fa
Mehul <mehulk2007@gmail.com>
2025-06-12 11:18:01 +0000

    Finish Basic Structure

---
+++ script.js
@@ -0,0 +1,55 @@
+const myLibrary = [];
+
+// Object constructor function
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages
+  this.read = read == true ? true : false;
+}
+
+function addBookToLibrary(bkname, bkauthor, bkpages, bkread) {
+  let book = new Book(bkname, bkauthor, bkpages, bkread);
+  book.id = crypto.randomUUID();
+  myLibrary.push(book);
+}
+
+addBookToLibrary("Harry Potter", "J K Rowling", 300, true);
+addBookToLibrary("Angels and Demons", "Dan Brown", 300, true);
+addBookToLibrary("The Alchemist", "Paulo Coelho", 300, true);
+
+
+let booksContainer = document.querySelector(".books-container");
+function displayBook(myLibrary) {
+  myLibrary.forEach(element => {
+    // creating elements
+    let book = document.createElement("div");
+    book.setAttribute("class", "book");
+    let title = document.createElement("h2");
+    title.setAttribute("class", "title");
+    let author = document.createElement("h2");
+    author.setAttribute("class", "author");
+    let pages = document.createElement("h2");
+    pages.setAttribute("class", "pages");
+    let status = document.createElement("h2");
+    status.setAttribute("class", "pages");
+    //assigning elements
+    title.textContent = element.name;
+    author.textContent = element.author;
+    pages.textContent = element.pages;
+    status.textContent = element.read;
+
+
+
+
+    //appending elemenets
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(status);
+    booksContainer.appendChild(book); 
+    
+  });
+}
+
+displayBook(myLibrary);
